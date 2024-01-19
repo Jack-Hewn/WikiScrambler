77af37546b13236b6e4cd4263c0843db38cb8f18
Jack-Hewn <67242053+Jack-Hewn@users.noreply.github.com>
2024-01-19 13:45:59 -0600

    Update wikiscrambler.js

--- wikiscrambler.js
+++ wikiscrambler.js
@@ -1,2 +1,18 @@
+npm install wikipedia
+
 var HTTPS = require('https');
 const wiki = require('wikipedia');
+
+(async () => {
+	try {
+		const searchResults = await wiki.search('Batma');
+		console.log(searchResults);
+		//Response of type @wikiSearchResult - contains results and optionally a suggestion
+		const newUrl = await wiki.setLang('fr');
+		console.log(newUrl);
+		//Returns the api url with language changed - use `languages()` method to see a list of available langs
+	} catch (error) {
+		console.log(error);
+		//=> Typeof wikiError
+	}
+})();
