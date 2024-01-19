71a96e0812100608105feec79982326191b420c1
Jack-Hewn <67242053+Jack-Hewn@users.noreply.github.com>
2024-01-19 17:55:56 -0600

    Update wikiscrambler.js

--- wikiscrambler.js
+++ wikiscrambler.js
@@ -8,9 +8,6 @@ import fetch from 'node-fetch';
 		let fetch = await import('node-fetch')
 		console.log(searchResults);
 		//Response of type @wikiSearchResult - contains results and optionally a suggestion
-		const newUrl = await wiki.setLang('en');
-		console.log(newUrl);
-		//Returns the api url with language changed - use `languages()` method to see a list of available langs
 	} catch (error) {
 		console.log(error);
 		//=> Typeof wikiError
