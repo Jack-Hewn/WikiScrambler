c1abbaaa1fd0b7151738e67aa12a0060f6f56835
Jack-Hewn <67242053+Jack-Hewn@users.noreply.github.com>
2024-01-19 14:09:56 -0600

    Update wikiscrambler.js

--- wikiscrambler.js
+++ wikiscrambler.js
@@ -5,7 +5,7 @@ const wiki = require('wikipedia');
 		const searchResults = await wiki.search('Batma');
 		console.log(searchResults);
 		//Response of type @wikiSearchResult - contains results and optionally a suggestion
-		const newUrl = await wiki.setLang('fr');
+		const newUrl = await wiki.setLang('en');
 		console.log(newUrl);
 		//Returns the api url with language changed - use `languages()` method to see a list of available langs
 	} catch (error) {
