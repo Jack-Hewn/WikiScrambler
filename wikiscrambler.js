950046c26af860b6f454312e33073f6627efe3b5
Jack-Hewn <67242053+Jack-Hewn@users.noreply.github.com>
2024-01-19 18:00:55 -0600

    Update wikiscrambler.js

--- wikiscrambler.js
+++ wikiscrambler.js
@@ -5,7 +5,6 @@ import fetch from 'node-fetch';
 (async () => {
 	try {
 		const searchResults = await wiki.search(generate());
-		let fetch = await import('node-fetch')
 		console.log(searchResults);
 		//Response of type @wikiSearchResult - contains results and optionally a suggestion
 	} catch (error) {
