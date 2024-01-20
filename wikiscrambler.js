0d671d62931939e14a086b08a7d26d3dbfa74a17
Jack-Hewn <67242053+Jack-Hewn@users.noreply.github.com>
2024-01-19 18:03:55 -0600

    Update wikiscrambler.js

--- wikiscrambler.js
+++ wikiscrambler.js
@@ -4,7 +4,7 @@ import { generate, count } from 'random-words';
 (async () => {
 	try {
 		const searchResults = await wiki.search(generate());
-		console.log(searchResults);
+		console.log('First' + searchResults);
 		//Response of type @wikiSearchResult - contains results and optionally a suggestion
 	} catch (error) {
 		console.log(error);
