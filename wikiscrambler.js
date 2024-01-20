efb73186cadfdca6b7f46a765b8200b1e06ee6b0
Jack-Hewn <67242053+Jack-Hewn@users.noreply.github.com>
2024-01-19 18:07:37 -0600

    Update wikiscrambler.js

--- wikiscrambler.js
+++ wikiscrambler.js
@@ -4,7 +4,19 @@ import { generate, count } from 'random-words';
 (async () => {
 	try {
 		const searchResults = await wiki.search(generate());
-		console.log('Starting point')
+		console.log('Starting point:')
+		console.log(searchResults);
+		//Response of type @wikiSearchResult - contains results and optionally a suggestion
+	} catch (error) {
+		console.log(error);
+		//=> Typeof wikiError
+	}
+})();
+
+(async () => {
+	try {
+		const searchResults = await wiki.search(generate());
+		console.log('Destination:')
 		console.log(searchResults);
 		//Response of type @wikiSearchResult - contains results and optionally a suggestion
 	} catch (error) {
